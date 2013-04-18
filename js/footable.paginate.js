ea82d8d1ecf85c47e8e7c55617129b150c71a163
Brad Vincent <bradvin@gmail.com>
2013-04-18 10:42:06 +0200

    refactored paging extension
    renamed options to better suit paging
    fetch default page size from data attrib on table
    renamed navigate function to createNavigation
    added in some checks incase the nav was left out the page
    prefix all classes with "footable" so that there are no conflicts with
    other css
    added showrow function to handle showing of rows
    changed paginate css to accomodate changes (also indented)

--- js/footable.paginate.js
+++ js/footable.paginate.js
@@ -1,11 +1,12 @@
+/// <reference path="../demo-group-headers.htm" />
 (function ($, w, undefined) {
   if (w.footable == undefined || w.footable == null)
     throw new Error('Please check and make sure footable.js is included in the page and is loaded prior to this script.');
 
   var defaults = {
     paginate: true,
-    increment: 10,
-    navigation: '.footable-nav'
+    pageSize: 10,
+    pageNavigation: '.footable-nav'
   };
 
   function Paginate() {
@@ -13,25 +14,26 @@
     p.name = 'Footable Paginate';
     p.pages = [];
     p.currentPage = 0;
+    p.pageSize = 0;
     
     p.init = function (ft) {
       if (ft.options.paginate == true) {
-        $(ft.table).bind({
+        var $table = $(ft.table), $tbody = $table.find('> tbody');
+        $table.bind({
           'footable_initialized': function (e) {
-            var $table = $(e.ft.table), $tbody = $table.find('> tbody');
-            p.input = $table.data('nav') || e.ft.options.navigation;
+            p.input = $table.data('page-navigation') || e.ft.options.pageNavigation;
+            p.pageSize = $table.data('page-size') || e.ft.options.pageSize;
             p.createPages(ft, $tbody);
-            p.navigate(ft, $tbody);
+            p.createNavigation(ft, $tbody);
             p.fillPage(ft, $tbody, 0);
           },
           'footable_sorted': function (e) {
-            var $table = $(e.ft.table), $tbody = $table.find('> tbody');
             p.createPages(ft, $tbody);
             p.fillPage(ft, $tbody, p.currentPage);
           },
           'footable_filtered' : function(e) {
-            var $table = $(e.ft.table), $tbody = $table.find('> tbody');
             p.createPages(ft, $tbody);
+            p.createNavigation(ft, $tbody);
             p.fillPage(ft, $tbody, p.currentPage);
           }
         });
@@ -40,7 +42,7 @@
     
     p.createPages = function (ft, tbody) {
       var pages = 1;
-      var pageCount = pages * ft.options.increment;
+      var pageCount = pages * p.pageSize;
       var page = [];
       var lastPage = [];
       p.pages = [];
@@ -50,46 +52,48 @@
         if (i === pageCount - 1) {
           p.pages.push(page);
           pages++;
-          pageCount = pages * ft.options.increment;
+          pageCount = pages * p.pageSize;
           page = [];
-        } else if (i >= rows.length - (rows.length % ft.options.increment)) {
+        } else if (i >= rows.length - (rows.length % p.pageSize)) {
           lastPage.push(row);
         }
       });
-      p.pages.push(lastPage);
+      if (lastPage.length>0) p.pages.push(lastPage);
     };
 
-    p.navigate = function (ft, tbody) {
+    p.createNavigation = function (ft, tbody) {
+      var $nav = $(p.input);
+      if ($nav.length == 0) return;
+      $nav.find('li').remove();
       if (p.pages.length > 0) {
-        var element = $(p.input);
-        element.append('<li class="arrow"><a href="#prev">&laquo;</a></li>');
+        
+        $nav.append('<li class="footable-page-arrow"><a href="#prev">&laquo;</a></li>');
         $.each(p.pages, function (i, page) {
           if (page.length > 0) {
-            element.append('<li class="page"><a href="#">' + (i + 1) + '</a></li>');
+            $nav.append('<li class="footable-page"><a href="#">' + (i + 1) + '</a></li>');
           }
         });
-        element.append('<li class="arrow"><a href="#next">&raquo;</a></li>');
+        $nav.append('<li class="footable-page-arrow"><a href="#next">&raquo;</a></li>');
       }
-      $(p.input + ' a').on('click', function (e) {
+      $nav.find('a').click(function (e) {
         e.preventDefault();
         if ($(this).attr('href') == '#prev') {
           if (p.currentPage > 0) {
             p.fillPage(ft, tbody, p.currentPage - 1);
           }
         } else if ($(this).attr('href') == '#next') {
-          if (p.currentPage < p.pages.length - 2) {
+          if (p.currentPage < p.pages.length - 1) {
             p.fillPage(ft, tbody, p.currentPage + 1);
-
           }
         } else {
           if (p.currentPage != ($(this).html() - 1)) {
             p.fillPage(ft, tbody, $(this).html() - 1);
           }
         }
-        $(p.input + ' li').removeClass('current');
-        $(p.input + ' li.page:eq(' + p.currentPage + ')').addClass('current');
+        $nav.find('li').removeClass('footable-page-current');
+        $nav.find('li.footable-page:eq(' + p.currentPage + ')').addClass('footable-page-current');
       });
-      $(p.input + ' li.page:eq(' + p.currentPage + ')').addClass('current');
+      $nav.find('li.footable-page:eq(' + p.currentPage + ')').addClass('footable-page-current');
     };
 
     p.fillPage = function (ft, tbody, pageNumber) {
@@ -97,12 +101,17 @@
 
       tbody.find('> tr:visible').hide();
       $(p.pages[pageNumber]).show().each(function () {
-        var $next = $(this).next();
-        if ($next.hasClass('footable-row-detail')) {
-          $next.show();
-          ft.createOrUpdateDetailRow(this);
-        }
+        p.showRow(this, ft);
       });
+    };
+    
+    p.showRow = function (row, ft) {
+      var $row = $(row), $next = $row.next(), $table = $(ft.table);
+      if ($table.hasClass('breakpoint') && $row.hasClass('footable-detail-show') && $next.hasClass('footable-row-detail')) {
+        $row.add($next).show();
+        ft.createOrUpdateDetailRow(row);
+      }
+      else $row.show();
     };
   };
 
