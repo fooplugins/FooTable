5bc08d92fffd73aaf782f484a10d9847af8745a5
Brad Vincent <bradvin@gmail.com>
2013-09-06 07:27:41 +0200

    added paging API

--- js/footable.paginate.js
+++ js/footable.paginate.js
@@ -7,8 +7,8 @@
         pageSize: 10,
         pageNavigation: '.pagination',
         firstText: '&laquo;',
-		previousText: '&lsaquo;',
-		nextText: '&rsaquo;',
+        previousText: '&lsaquo;',
+        nextText: '&rsaquo;',
         lastText: '&raquo;'
     };
 
@@ -17,8 +17,8 @@
         this.pageNavigation = $table.data('page-navigation') || ft.options.pageNavigation;
         this.pageSize = $table.data('page-size') || ft.options.pageSize;
         this.firstText = $table.data('page-first-text') || ft.options.firstText;
-		this.previousText = $table.data('page-previous-text') || ft.options.previousText;
-		this.nextText = $table.data('page-next-text') || ft.options.nextText;
+        this.previousText = $table.data('page-previous-text') || ft.options.previousText;
+        this.nextText = $table.data('page-next-text') || ft.options.nextText;
         this.lastText = $table.data('page-last-text') || ft.options.lastText;
         this.currentPage = 0;
         this.pages = [];
@@ -32,26 +32,30 @@
         p.init = function (ft) {
             if (ft.options.paginate === true) {
                 if ($(ft.table).data('page') === false) return;
+                p.footable = ft;
                 $(ft.table).bind({
                     'footable_initialized': function () {
                         ft.pageInfo = new pageInfo(ft);
-						ft.raise('footable_setup_paging');
+                        ft.raise('footable_setup_paging');
                     },
                     'footable_row_removed footable_redrawn footable_sorted footable_filtered footable_setup_paging': function () {
                         if (ft.pageInfo) {
-                            p.setupPaging(ft);
+                            p.setupPaging();
                         }
                     }
-                });
+                })
+                    //save the filter object onto the table so we can access it later
+                    .data('footable-paging', p);
             }
         };
 
-		p.setupPaging = function(ft) {
-			var $tbody = $(ft.table).find('> tbody');
-			p.createPages(ft, $tbody);
-			p.createNavigation(ft, $tbody);
-			p.fillPage(ft, $tbody, ft.pageInfo.currentPage);
-		};
+        p.setupPaging = function () {
+            var ft = p.footable,
+                $tbody = $(ft.table).find('> tbody');
+            p.createPages(ft, $tbody);
+            p.createNavigation(ft, $tbody);
+            p.fillPage(ft, $tbody, ft.pageInfo.currentPage);
+        };
 
         p.createPages = function (ft, tbody) {
             var pages = 1;
@@ -85,34 +89,36 @@
 
         p.createNavigation = function (ft, tbody) {
             var $nav = $(ft.table).find(ft.pageInfo.pageNavigation);
-			//if we cannot find the navigation control within the table, then try find it outside
-			if ($nav.length === 0) {
-				$nav = $(ft.pageInfo.pageNavigation);
-				//if the navigation control is inside another table, then get out
-				if ($nav.parents('table:first') !== $(ft.table)) return;
-				//if we found more than one navigation control, write error to console
-				if ($nav.length > 1 && ft.options.debug === true) console.error('More than one pagination control was found!');
-			}
-			//if we still cannot find the control, then don't do anything
+            //if we cannot find the navigation control within the table, then try find it outside
+            if ($nav.length === 0) {
+                $nav = $(ft.pageInfo.pageNavigation);
+                //if the navigation control is inside another table, then get out
+                if ($nav.parents('table:first') !== $(ft.table)) return;
+                //if we found more than one navigation control, write error to console
+                if ($nav.length > 1 && ft.options.debug === true) console.error('More than one pagination control was found!');
+            }
+            //if we still cannot find the control, then don't do anything
             if ($nav.length === 0) return;
-			//if the nav is not a UL, then find or create a UL
-			if (!$nav.is('ul')) {
-				if ($nav.find('ul:first').length === 0) { $nav.append('<ul />'); }
-				$nav = $nav.find('ul');
-			}
+            //if the nav is not a UL, then find or create a UL
+            if (!$nav.is('ul')) {
+                if ($nav.find('ul:first').length === 0) {
+                    $nav.append('<ul />');
+                }
+                $nav = $nav.find('ul');
+            }
             $nav.find('li').remove();
             var info = ft.pageInfo;
             info.control = $nav;
             if (info.pages.length > 0) {
-                $nav.append('<li class="footable-page-arrow"><a data-page="first" href="#first">'+ft.pageInfo.firstText+'</a>');
-                $nav.append('<li class="footable-page-arrow"><a data-page="prev" href="#prev">'+ft.pageInfo.previousText+'</a></li>');
+                $nav.append('<li class="footable-page-arrow"><a data-page="first" href="#first">' + ft.pageInfo.firstText + '</a>');
+                $nav.append('<li class="footable-page-arrow"><a data-page="prev" href="#prev">' + ft.pageInfo.previousText + '</a></li>');
                 $.each(info.pages, function (i, page) {
                     if (page.length > 0) {
                         $nav.append('<li class="footable-page"><a data-page="' + i + '" href="#">' + (i + 1) + '</a></li>');
                     }
                 });
-                $nav.append('<li class="footable-page-arrow"><a data-page="next" href="#next">'+ft.pageInfo.nextText+'</a></li>');
-                $nav.append('<li class="footable-page-arrow"><a data-page="last" href="#last">'+ft.pageInfo.lastText+'</a></li>');
+                $nav.append('<li class="footable-page-arrow"><a data-page="next" href="#next">' + ft.pageInfo.nextText + '</a></li>');
+                $nav.append('<li class="footable-page-arrow"><a data-page="last" href="#last">' + ft.pageInfo.lastText + '</a></li>');
             }
             $nav.find('a').click(function (e) {
                 e.preventDefault();
@@ -131,7 +137,7 @@
                 }
                 p.paginate(ft, newPage);
             });
-			p.setPagingClasses($nav, info.currentPage, info.pages.length);
+            p.setPagingClasses($nav, info.currentPage, info.pages.length);
         };
 
         p.paginate = function (ft, newPage) {
@@ -149,17 +155,17 @@
             }
         };
 
-		p.setPagingClasses = function(nav, currentPage, pageCount) {
+        p.setPagingClasses = function (nav, currentPage, pageCount) {
             nav.find('li.footable-page > a[data-page=' + currentPage + ']').parent().addClass('active');
-			if (currentPage >= pageCount - 1) {
-				nav.find('li.footable-page-arrow > a[data-page="next"]').parent().addClass('disabled');
+            if (currentPage >= pageCount - 1) {
+                nav.find('li.footable-page-arrow > a[data-page="next"]').parent().addClass('disabled');
                 nav.find('li.footable-page-arrow > a[data-page="last"]').parent().addClass('disabled');
-			}
-			if (currentPage < 1) {
+            }
+            if (currentPage < 1) {
                 nav.find('li.footable-page-arrow > a[data-page="first"]').parent().addClass('disabled');
-				nav.find('li.footable-page-arrow > a[data-page="prev"]').parent().addClass('disabled');
-			}
-		};
+                nav.find('li.footable-page-arrow > a[data-page="prev"]').parent().addClass('disabled');
+            }
+        };
 
         p.fillPage = function (ft, tbody, pageNumber) {
             ft.pageInfo.currentPage = pageNumber;
