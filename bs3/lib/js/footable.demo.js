91d113eef88dd0d2f5870c499858dcdc4a82f60c
Mark Constable <markc@renta.net>
2013-10-06 15:36:20 +1000

    Disable extra event handler in footable.demo.js

--- bs3/lib/js/footable.demo.js
+++ bs3/lib/js/footable.demo.js
@@ -13,14 +13,19 @@ $(function () {
         $.get(e.target.pathname||'README.md', function(res) {
             if ('md' == e.target.pathname.split('.').pop()) {
                 $('#content').html(marked(res));
-                $('#content').click(function (e2) {
-                    e2.preventDefault();
+/*
+// this is trying to add a click event just to the content area only but
+// clashes with the demos
+
+                $('#content').click(function (e2)
+                      e2.preventDefault();
                     $.get(e2.target.pathname, function(res) {
                         if ('md' == e2.target.pathname.split('.').pop())
                             res = marked(res);
                         $('#content').html(res);
                     });
                 });
+*/
             } else {
                 $('#content').html(res);
             }
