29889327bfc936ffe654c14300dc76a04c491d23
Oliver Rivett-Carnac <oliverrc@gmail.com>
2013-10-09 11:15:19 +0200

    Added Striping Template - Initial Checkin
    Currently has no CSS or documentation and is a first and mostly untested
    commit

---
+++ js/footable.striping.js
@@ -0,0 +1,38 @@
+﻿(function ($, w, undefined) {
+  if (w.footable === undefined || w.foobox === null)
+    throw new Error('Please check and make sure footable.js is included in the page and is loaded prior to this script.');
+
+  var defaults = {
+    striping: {
+      enabled: true
+    },
+    classes: {
+      striping: {
+        odd: 'footable-odd',
+        even: 'footable-even'
+      }
+    },
+  };
+
+  function Striping() {
+    var p = this;
+    p.name = 'Footable Striping';
+    p.init = function (ft) {
+      p.footable = ft;
+      $(ft.table)
+        .unbind('striping')
+        .bind({
+          'footable_initialized.striping footable_row_removed.striping footable_redrawn.striping footable_sorted.striping footable_filtered.striping': function () {
+            p.setupStriping();
+          }
+        });
+    };
+
+    p.setupStriping = function () {
+      alert('Strip');
+    };
+  }
+
+  w.footable.plugins.register(Striping, defaults);
+
+})(jQuery, window);
