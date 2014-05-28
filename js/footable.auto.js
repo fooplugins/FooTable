698f4c035c1641f723d441bdd79211db2e7ff5cb
Shinrai <tronox@hotmail.com>
2014-05-28 07:41:22 -0700

    Create footable.auto.js
    Drag and drop file to make all footables automatically dynamic. Meaning you never have to add extra code to your tables. Just follow table standard, IE thead + th, tbody + td.

---
+++ js/footable.auto.js
@@ -0,0 +1,115 @@
+function autoadjustfootableforthwidth3(element) {
+    var r = false;
+    
+    jQuery(element).each(function(){
+        var droplast = false;
+		var reform = false;
+		var position1 = false;
+		var position2 = false;
+		jQuery(this).find('thead tr th:visible').each(function(){
+			position1 = jQuery(this).children('div').position();
+			position2 = jQuery(this).children('span').position();
+			if (position1 && position2){
+				if (position1.top != position2.top){
+					droplast = true;
+				}
+			}
+		});
+		if (droplast){
+			jQuery(this).find('thead tr th:visible').last().hide().attr('data-aafw','2').data('hide','all').attr('data-hide','all').prop('data-hide','all');
+            r = true;
+		}
+	});
+    return r;
+}
+
+function autoadjustfootables() {
+    jQuery('table.footable').find('> thead tr th').removeAttr('data-hide').data('hide','').prop('data-hide',false).show();
+    jQuery('table.footable').each(function(i){
+        var width = parseInt(jQuery(this).parent().innerWidth(),10);
+        var lwidth = 0;
+        jQuery(this).find('thead tr th').each(function(index){
+            var twidth = 0;
+            var swidth = 0;
+            var dwidth = jQuery(this).find('div').outerWidth();
+            jQuery(this).find('div').attr('data-swidth',dwidth);
+            if (jQuery(this).find('span')){
+                swidth = jQuery(this).find('div').outerWidth();
+                jQuery(this).find('span').attr('data-swidth',swidth);
+            }
+            twidth = twidth + swidth;
+            var padding = parseInt(jQuery(this).css('padding-left'),10) + parseInt(jQuery(this).css('padding-right'),10);
+            jQuery(this).attr('data-twidth',twidth).attr('data-padding',padding);
+            lwidth = lwidth + twidth + padding;
+            if (width > lwidth) {
+                jQuery(this).removeAttr('data-hide').data('hide','').prop('data-hide',false).show();
+            } else {
+                jQuery(this).attr('data-aaf','1').hide().attr('data-hide','all').data('hide','all').prop('data-hide','all');
+            }
+        });
+        autoadjustfootableforthwidth3(this);
+        var visible = jQuery(this).find('thead tr th:visible').length;
+        var total = jQuery(this).find('thead tr th').length;
+        console.log('visible: '+visible);
+        console.log('total: '+total);
+        if (visible == total && jQuery(this).find('tbody tr.footable-row-detail')){
+            jQuery(this).removeClass('breakpoint');
+        } else {
+            jQuery(this).addClass('breakpoint');
+            jQuery(this).find('tbody tr.footable-row-detail').show();
+        }
+        reformfootable(this);
+    });
+}
+
+function reformfootable(element){
+    jQuery(element).trigger('footable_readdata').trigger('footable_redraw').trigger('footable_resize');
+    return true;
+}
+
+jQuery(window).resize(function(){
+    waitForFinalEvent(function(){autoadjustfootables();},100,'autoadjustfootables');
+});
+
+jQuery(document).ready(function(){
+    setTimeout(function(){
+        jQuery('table.footable thead tr th div').each(function(){
+            var text = jQuery(this).html();
+            jQuery(this).html(text.replace(/ /g, " "));
+        });
+		
+        waitForFinalEvent(function(){autoadjustfootables();},100,'autoadjustfootables');
+		
+        jQuery('table').each(function(){
+            if (!jQuery(this).hasClass('search')){
+                jQuery(this).find('.footable-filter-container').remove();
+                jQuery(this).prev('.footable-filter-container').remove();
+            }
+        });
+        footableremovesortswhennotneeded();
+    },100);
+});
+
+function footableremovesortswhennotneeded() {
+    jQuery('table.footable').each(function(){
+        var count = jQuery(this).find('tbody tr').length;
+        if (count <= 1){
+            jQuery(this).find('thead tr th').removeClass('footable-sortable').find('span.footable-sort-indicator').remove();
+        }
+    });
+}
+
+if (typeof waitForFinalEvent == "undefined"){
+	var waitForFinalEvent = (function () {
+		var timers = {};
+		return function (callback, ms, uniqueId) {
+			if (!uniqueId) {
+				uniqueId = "Don't call this twice without a uniqueId";
+			}
+			if (timers[uniqueId]) {
+				clearTimeout (timers[uniqueId]);
+			}
+			timers[uniqueId] = setTimeout(callback, ms);
+		};
+	})();
+}
