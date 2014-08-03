bc5e75d3142e466030c87646b75921e2d7c3344b
Chris Laskey <contact@chrislaskey.com>
2014-08-03 10:02:11 -0400

    Added complete storage object to Footable Memory plugin

--- js/footable.memory.js
+++ js/footable.memory.js
@@ -14,14 +14,115 @@
         }
     };
 
-    var storage = {
-        get: function(index){
+    var storage = (function($){
 
-        },
-        set: function(index, item){
+        'use strict';
 
-        }
-    };
+        /**
+         * Stores footable bookmarkable data in a cookie
+         *
+         * By default will store each page in its own cookie.
+         * Supports multiple FooTables per page.
+         * Supports JS frameworks that use hashmap URLs (AngularJS, Ember, etc).
+         *
+         * For example take an example application:
+         *
+         *     http://example.com/application-data (2 FooTables on this page)
+         *     http://example.com/application-data/#/details (1 FooTable on this page)
+         *     http://example.com/other-data (1 FooTable on this page)
+         *
+         * Would be stored like this:
+         *
+         *     cookie['/application-data'] = {
+         *         '/': {
+         *             1: {
+         *                 key1: value1,
+         *                 key2: value2
+         *             },
+         *             2: {
+         *                 key1: value1,
+         *                 key2: value2
+         *             }
+         *         },
+         *         '#/details': {
+         *             1: {
+         *                 key1: value1,
+         *                 key2: value2
+         *             }
+         *         }
+         *     };
+         *
+         *     cookie['/other-data'] = {
+         *         '/': {
+         *             1: {
+         *                 key1: value1,
+         *                 key2: value2
+         *             },
+         *         }
+         *     }
+         *
+         */
+
+        $.cookie.json = true;
+
+        var days_to_keep_data = 7;
+
+        var path_page = function(){
+            return location.pathname;
+        };
+
+        var path_subpage = function(){
+            return location.hash || '/';
+        };
+
+        var get_data = function(){
+            var page = path_page(),
+                data = $.cookie(page);
+
+            return data || {};
+        };
+
+        var get_table = function(index){
+            var subpage = path_subpage(),
+                data = get_data();
+
+            if( data[subpage] && data[subpage][index] ){
+                return data[subpage][index];
+            } else {
+                return {};
+            }
+        };
+
+        var set = function(index, item){
+            var page = path_page(),
+                subpage = path_subpage(),
+                data = get_data(),
+                options;
+
+            if( !data[subpage] ){
+                data[subpage] = {};
+            }
+
+            data[subpage][index] = item;
+
+            options = {
+                path: page,
+                expires: days_to_keep_data
+            }
+
+            $.cookie(page, data, options);
+        };
+
+        return {
+            get: function(index){
+                return get_table(index);
+            },
+            set: function(index, item){
+                set(index, item);
+            }
+        };
+
+    })($);
 
     var state = {
         get: function(ft){
