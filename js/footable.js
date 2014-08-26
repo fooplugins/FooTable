2096bb86fe22beefdc897dae1bfc100b4cf3d6da
Brad Vincent <bradvin@gmail.com>
2014-08-26 10:35:54 +0200

    bumped version and updated bower.json

--- js/footable.js
+++ js/footable.js
@@ -1,6 +1,6 @@
 ﻿/*!
  * FooTable - Awesome Responsive Tables
- * Version : 2.0.1.4
+ * Version : 2.0.1.5
  * http://fooplugins.com/plugins/footable-jquery/
  *
  * Requires jQuery - http://jquery.com/
