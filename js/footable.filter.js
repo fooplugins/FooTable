74b9d8f97d95a74b34674af1e9346606ae963e02
CraigHarley <craigharley2@googlemail.com>
2015-10-21 11:34:16 +0100

    Update footable.filter.js

--- js/footable.filter.js
+++ js/footable.filter.js
@@ -104,6 +104,8 @@
           if (event.clear) {
                 p.clearFilter();
             } else {
+                //event.filter; //filters on multiple words appearing, not a single string
+
                 $table.find('> tbody > tr').hide().addClass('footable-filtered');
                 var rows = $table.find('> tbody > tr:not(.footable-row-detail)');
                     if (event.filter && event.filter.length > 0) {
