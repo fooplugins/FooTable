6ec0d9f6c81351daa12458fc79f8ca78dfcb9213
steveush <steveush@gmail.com>
2016-10-06 15:36:06 +0200

    Updated the `min` option default value to `1` for the filtering component.

--- src/js/components/filtering/extends/FooTable.Defaults.js
+++ src/js/components/filtering/extends/FooTable.Defaults.js
@@ -5,7 +5,7 @@
 	 * @prop {boolean} enabled=false - Whether or not to allow filtering on the table.
 	 * @prop {({name: string, query: (string|FooTable.Query), columns: (Array.<string>|Array.<number>|Array.<FooTable.Column>)}|Array.<FooTable.Filter>)} filters - The filters to apply to the current {@link FooTable.Rows#array}.
 	 * @prop {number} delay=1200 - The delay in milliseconds before the query is auto applied after a change (any value equal to or less than zero will disable this).
-	 * @prop {number} min=3 - The minimum number of characters allowed in the search input before it is auto applied.
+	 * @prop {number} min=1 - The minimum number of characters allowed in the search input before it is auto applied.
 	 * @prop {string} space="AND" - Specifies how whitespace in a filter query is handled.
 	 * @prop {string} placeholder="Search" - The string used as the placeholder for the search input.
 	 * @prop {string} dropdownTitle=null - The title to display at the top of the search input column select.
@@ -18,7 +18,7 @@
 		enabled: false,
 		filters: [],
 		delay: 1200,
-		min: 3,
+		min: 1,
 		space: 'AND',
 		placeholder: 'Search',
 		dropdownTitle: null,
