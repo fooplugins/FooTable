2fe3cc959eee0dc9a7f2e7478556fda97d45618f
steveush <steveush@gmail.com>
2017-03-16 17:03:58 +0200

    Fixed the `FooTable.Table#_construct` method which was not returning a promise as it should have been doing.

--- src/js/components/state/extends/FooTable.Table.js
+++ src/js/components/state/extends/FooTable.Table.js
@@ -3,7 +3,7 @@
 	// hook into the _construct method so we can add the state property to the table.
 	F.Table.extend('_construct', function(ready){
 		this.state = this.use(FooTable.State);
-		this._super(ready);
+		return this._super(ready);
 	});
 
 	// hook into the _preinit method so we can trigger a plugin wide read state operation.
